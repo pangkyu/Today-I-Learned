c42c5264634349e36d1ff8ce12da239a8e6b12c8
Bae seong-kyu <baesense@naver.com>
2023-03-02 16:25:10 +0900

    [unrated] Title: 기사단원의 무기, Time: 3.37 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,27 @@
+function solution(number, limit, power) {
+    const nums = [];
+    let result = 0; 
+    
+    for (let cur = 1; cur <= number; cur++) {
+        let divisor = 0;
+    
+        for (let i = 1; i <= cur / 2; i++) {
+        if (cur % i === 0) {
+            divisor += 1;
+        }
+    }
+    nums.push(divisor + 1);
+  }
+ 
+    nums.map((item) => {
+        if(item > limit){
+            result += power;
+        }else{
+            result += item;
+        }
+      
+    });
+    return result;
+    
+    
+}
