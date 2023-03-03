55f052be294fc5c2dfef42a27bd603ee3916811d
Bae seong-kyu <baesense@naver.com>
2023-03-03 21:50:08 +0900

    [level 2] Title: 올바른 괄호, Time: 2.36 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,14 +1,14 @@
 function solution(s){
     let stackCount = 0; 
     
-//     for(let i = 0; i < s.length; i++){
-//         s[i] === '(' ? stackCount++ : stackCount--;
-//         if(stackCount < 0) return false;
-//     }
-    for(let i of s) {
-        i === '(' ? stackCount++ : stackCount--;
+    for(let i = 0; i < s.length; i++){
+        s[i] === '(' ? stackCount++ : stackCount--;
         if(stackCount < 0) return false;
     }
+//     for(let i of s) {
+//         i === '(' ? stackCount++ : stackCount--;
+//         if(stackCount < 0) return false;
+//     }
     
     return stackCount === 0 ? true : false;
 }
